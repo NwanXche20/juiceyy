5655c8f1e8de9100d31b9977add15d26006f53f5
Nwaneri Uchechukwu Chigozie <uchenwaneri27@gmail.com>
2024-01-18 17:14:57 +0100

    added website title

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <title>Juiceyy</title>
         <link rel="shortcut icon" href="/favicon.ico" sizes="any" />
         <link
           rel="icon"
